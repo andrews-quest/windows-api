f88d153e8cf12283d56f6e9efb3c8cf5344d6a71
Andrii Roshkovan <andrii.roshkovan@gmail.com>
2024-05-22 18:58:14 +0200

    Created a custom Window class with a classy background colour.

--- a_window.cpp
+++ a_window.cpp
@@ -1,11 +1,39 @@
 #include <windows.h>
 
-int WINAPI WinMain (HINSTANCE hinst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow){
-MessageBoxA(
-        NULL,
-        (LPCSTR)"Resource not available\nDo you want to try again?",
-        (LPCSTR)"Account Details",
-        MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2
-    );
+
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
+{
+    WNDCLASSW wc = {0};
+    wc.hbrBackground =  CreateSolidBrush(RGB(199, 159, 100));
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hInstance = hInst;
+    wc.lpszClassName = L"myWindowClass";
+    wc.lpfnWndProc = WindowProcedure;
+
+    if(!RegisterClassW(&wc))
+        return -1;
+
+    CreateWindowW(L"myWindowClass", L"Amasing Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500, NULL, NULL, NULL, NULL);
+
+    MSG msg = {0};
+
+    while(GetMessage(&msg, NULL, 0, 0) ){
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
     return 0;
+}
+
+
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp){
+    switch (msg)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            DefWindowProcW(hWnd, msg, wp, lp);
+    }
 }
